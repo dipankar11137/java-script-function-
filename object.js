5657dfcf275a2f9d7e5ce2e1fdd782419c965126
Dipankar <dipankar15-11137@diu.edu.bd>
2022-01-29 12:24:44 +0600

    second commit

---
+++ object.js
@@ -0,0 +1,8 @@
+var student1 = { id: 115, name: 'Abir Abul', marks: 81, phone: 775755645, height: 5.5 };
+
+var mobile = {
+    color: 'black',
+    Price: 1900,
+    screeSize: 5,
+    storage: '16gb'
+};
